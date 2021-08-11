23f63462bd5744596f79ea65920680b91c194961
LyubovP <lyubovpidoshva@gmail.com>
2021-08-11 18:53:07 +0000

    Finish blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,12 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   def index
     @blogs = Blog.all
   end
 
   def show
+    # @blog = current_user.blogs.find(params[:id])
     @blog = Blog.find(params[:id])
   end
 
@@ -16,10 +19,9 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.create(blog_params)
-    
+    @blog = current_user.blogs.build(blog_params)
     if @blog.save
-      redirect_to @blog
+      redirect_to root_url
     else
       render :new
     end
@@ -29,7 +31,7 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
     
     if @blog.update(blog_params)
-    redirect_to @blog
+    redirect_to root_url
     
     else
      render :edit
@@ -48,5 +50,9 @@ class BlogsController < ApplicationController
   
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,6 +7,12 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Blog < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
+  
 end
